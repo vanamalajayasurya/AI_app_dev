282b3bbd2cbcf2ce5754bcae5070e243fb1eaefc
jayaSurya <vanamalajayasurya126@gmail.com>
2026-09-02 13:14:38 +0530

    DAY 1 is completed

---
+++ AI_python_foundations/hello.py
@@ -0,0 +1,19 @@
+print("Hello, World!")
+print("Hello", "hi", "greetings")
+print("HELLO MY NAME IS JAYASURYA")
+
+# dynamic typing
+name = "Jayasurya"
+print(type(name))
+# integer
+age = 22
+print(type(age))
+# float
+weight = 70.5
+print(type(weight))
+# boolean
+is_student = True
+print(type(is_student))
+
+grade = "A"
+print(type(grade))
