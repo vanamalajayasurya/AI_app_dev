f47921592dee6e60f06763b92817cc03a92c9915
jayaSurya <vanamalajayasurya126@gmail.com>
2026-09-07 01:04:57 +0530

    Day 4: Python Data Structures & Debugging Challenge

---
+++ AI_python_foundations/Task 4.py
@@ -0,0 +1,10 @@
+students = ["Ravi", "Anil", "Kiran", "Suresh"]
+
+
+for student in students:
+    if student == "Kiran":
+        print("Student found:", student)
+        break
+
+else:
+    print("Student not found") 
